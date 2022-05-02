979940d37e4bb8ae86029218f3f7748abc9c6a3a
mahelhelou <mahelhelou@gmail.com>
2022-05-02 03:13:15 +0300

    Finished the useState tutorial

--- src/App.js
+++ src/App.js
@@ -2,22 +2,18 @@ import React from 'react'
 import './App.css'
 
 // States
-import './_hooks/UseState/UseState'
-import './_hooks/Hello'
-import './_hooks/UseState/LoginForm'
-import StateTutorials from './_hooks/UseState'
-// import ReducerTutorials from './hooks/UseReducer/UseReducer'
-// import Garage from './w3schools/Garage'
-// import TodoApp from './w3schools/TodoApp'
-// import Counter from './w3schools/Counter'
+import './hooks/UseState/UseState'
+import './hooks/UseState/LoginForm'
+import { LoginFormC, LoginFormF } from './hooks/UseState/LoginForm'
+import StateTutorials from './hooks/UseState/UseState'
 
 function App() {
-	// return <ReducerTutorials />
-	// return <Garage />
-	// return <TodoApp />
 	return (
 		<>
+			{/* States components */}
 			<StateTutorials />
+			<LoginFormC />
+			<LoginFormF />
 		</>
 	)
 }
--- src/hooks/UseState/LoginForm.js
+++ src/hooks/UseState/LoginForm.js
@@ -1,11 +1,20 @@
 import React, { useState } from 'react'
 
+/**
+ * Class based component
+ * Old way of using component's state
+ * Define initial states in an object
+ */
 export class LoginFormC extends React.Component {
 	state = {
 		email: '',
 		passsword: ''
 	}
 
+	/**
+	 * Don't use function keyword!
+	 * Pass the value of new state as an object
+	 */
 	hanldeEmail = e => {
 		this.setState({
 			email: e.target.value
@@ -14,26 +23,37 @@ export class LoginFormC extends React.Component {
 
 	hanldePassword = e => {
 		this.setState({
-			passsword: e.target.value
+			password: e.target.value
 		})
 	}
 
+	/**
+	 * Use (this) keyword to render the initial state
+	 */
 	render() {
 		return (
 			<div>
 				<h2>Class Example</h2>
 				<input value={this.state.email} onChange={this.hanldeEmail} placeholder='email' />
-
 				<input value={this.state.password} onChange={this.hanldePassword} placeholder='password' />
+
+				<h3>What I Typed?</h3>
+				<h5>Email: {this.state.email}</h5>
+				<h5>Password: {this.state.password}</h5>
 			</div>
 		)
 	}
 }
 
 export function LoginFormF() {
+	/**
+	 * Initial states
+	 * Set the initial state and the function that updates the state
+	 */
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
 
+	// Use the function to update the state (With function keyword)
 	function hanldeEmail(e) {
 		setEmail(e.target.value)
 	}
@@ -45,10 +65,12 @@ export function LoginFormF() {
 	return (
 		<div>
 			<h2>Function Example</h2>
-
 			<input value={email} onChange={hanldeEmail} placeholder='email' />
-
 			<input value={password} onChange={hanldePassword} placeholder='password' />
+
+			<h3>What's I Typed?</h3>
+			<h5>Email: {email}</h5>
+			<h5>Password: {password}</h5>
 		</div>
 	)
 }
